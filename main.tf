300c6fea19c470ef2108d6d2ad08d64519ccd73c
nitish1507 <shiva11421@gmail.com>
2026-08-16 02:54:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,7 @@ resource "azurerm_resource_group" "anuj1" {
 
 resource "azurerm_resource_group" "abcd" {
   name     = "pathshala-rg"
+}
 resource "azurerm_resource_group" "anuj2" {
   name     = "rg-pen"
   location = "West Europe"
