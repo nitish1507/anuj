42a85985a8024a03a8f04f347daa117c6cbbd71b
nitish1507 <shiva11421@gmail.com>
2026-08-17 13:08:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,16 +16,3 @@ resource "azurerm_resource_group" "anuj" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "anuj1" {
-  name     = "anuj-rg2"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "abcd" {
-  name     = "anuj-rg3"
-location = "West Europe"
-}
-resource "azurerm_resource_group" "anuj2" {
-  name     = "anuj-rg4"
-  location = "West Europe"
-}
