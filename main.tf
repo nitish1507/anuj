d0e2f6af6e59c5a878e4ada514ef311305a31acd
Nitish Kumar <shiva11421@gmail.com>
2026-09-15 02:46:14 +0530

    sdfg

--- main.tf
+++ main.tf
@@ -25,3 +25,18 @@ resource "azurerm_storage_account" "stdemorg2" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_account" "stdemorgddd2" {
+  name                     = "stdevodddpsdemo003"
+  location                 = "Central India"
+  resource_group_name      = "rg-dedddvops-demo"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "stdddddddadaemorg2" {
+  name                     = "stdevoasfrepsdemo003"
+  location                 = "Central India"
+  resource_group_name      = "rg-dedffdsvops-demo"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
