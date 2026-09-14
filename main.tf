c106e1e296891b5f2d01443c6a2fbacf16929095
Nitish Kumar <shiva11421@gmail.com>
2026-09-15 02:28:32 +0530

    mama

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_storage_account" "stdemorg" {
+  name                     = "stdevopsdemo001"
+  location                 = "Central India"
+  resource_group_name      = "rg-devops-demo"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "stdemorg1" {
+  name                     = "stdevopsdemo002"
+  location                 = "Central India"
+  resource_group_name      = "rg-devops-demo"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "stdemorg2" {
+  name                     = "stdevopsdemo003"
+  location                 = "Central India"
+  resource_group_name      = "rg-devops-demo"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "stdemorg3" {
+  name                     = "stdevopsdemo004"
+  location                 = "Central India"
+  resource_group_name      = "rg-devops-demo"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
